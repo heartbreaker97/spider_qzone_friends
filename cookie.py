22c8fa3f87afc90ff680b0f81d43ec417c0d666e
yng <1318074793@qq.com>
2018-08-07 10:57:08 +0800

    修改了webdriver环境变量

--- cookie.py
+++ cookie.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from time import sleep
 import json
 
-chromedriver = 'C:/Users/13180/AppData/Local/Google/Chrome/Application/chromedriver.exe'
-driver = webdriver.Chrome(chromedriver)
-driver.get('https://user.qzone.qq.com/1318074793/main')
+#chromedriver = 'C:/Users/13180/AppData/Local/Google/Chrome/Application/chromedriver.exe'
+#driver = webdriver.Chrome(chromedriver)
+driver = webdriver.Chrome()
+driver.get('https://user.qzone.qq.com/你的qq/main')
 driver.switch_to.frame('login_frame')
 #找到账号密码登陆的地方
 driver.find_element_by_id('switcher_plogin').click()
-driver.find_element_by_id('u').send_keys('1318074793')
-driver.find_element_by_id('p').send_keys('962464myr')
+driver.find_element_by_id('u').send_keys('你的qq')
+driver.find_element_by_id('p').send_keys('你的密码')
 driver.find_element_by_id('login_button').click()
 #保存本地的cookie
 sleep(1)
